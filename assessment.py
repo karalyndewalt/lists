3d98c0c93fba40b4ba648eb3459f859b697cc672
ahm lee <ahmsolo@gmail.com>
2016-07-09 12:31:40 -0700

    submitting lists assessment

---
+++ assessment.py
@@ -0,0 +1,165 @@
+"""List Assessment 
+
+Edit the functions until all of the doctests pass when
+you run this file.
+"""
+
+
+def all_odd(numbers):
+    """Return a list of only the odd numbers in the input list.
+
+    For example::
+
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+    """
+
+    # initializing an empty list to append odd numbers to
+    odd_numbers = []
+
+    for number in numbers:
+        # if the remainder of the number modulo 2 is 1, that means it's
+        # an odd number
+        if number % 2 == 1:
+            odd_numbers.append(number)
+
+    return odd_numbers
+
+
+def print_indices(items):
+    """Print index of each item in list, followed by item itself.
+
+    Do this without using a "counting variable" --- that is, don't
+    do something like this::
+
+        count = 0
+        for item in list:
+            print count
+            count = count + 1
+
+    Output should look like this::
+
+        >>> print_indices(["Toyota", "Jeep", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Volvo
+    """
+
+    for item in items:
+        print items.index(item), item
+
+
+def foods_in_common(foods1, foods2):
+    """Find foods in common.
+
+    Given 2 lists of foods, return the items that are in common
+    between the two, sorted alphabetically.
+
+    **NOTE**: for this problem, you're welcome to use any of the
+    Python data structures you've been introduced to (not just
+    lists). Is there another that would be a good idea?
+
+    For example::
+
+        >>> foods_in_common(
+        ...     ["cheese", "bagel", "cake", "kale"],
+        ...     ["hummus", "beets", "bagel", "lentils", "kale"]
+        ... )
+        ['bagel', 'kale']
+
+    If there are no foods in common, return an empty list::
+
+        >>> foods_in_common(
+        ...     ["lamb", "chili", "cheese"],
+        ...     ["cake", "ice cream"]
+        ... )
+        []
+
+    """
+
+    # initializing an empty list to append common foods to
+    common_foods_list = []
+
+    for food in foods1:
+        # if this food item from foods1 exists in the foods2, append it to the
+        # common_foods_list
+        if food in foods2:
+            common_foods_list.append(food)
+        elif len(foods1) == 0:
+            return []
+
+    return common_foods_list
+
+
+def every_other_item(items):
+    """Return every other item in `items`, starting at first item.
+
+    For example::
+
+       >>> every_other_item([1, 2, 3, 4, 5, 6])
+       [1, 3, 5]
+
+       >>> every_other_item(
+       ...   ["you", "z", "are", "z", "good", "z", "at", "x", "code"]
+       ... )
+       ['you', 'are', 'good', 'at', 'code']
+    """
+
+    # initializing an empty list to append every other item to
+    every_other_item = []
+
+    for item in items:
+        if items.index(item) == 0:
+            every_other_item.append(item)
+        elif items.index(item) % 2 == 0:
+            every_other_item.append(item)
+    # sorry - i can't seem to get rid of this white space           
+    return every_other_item
+
+
+def largest_n_items(items, n):
+    """Return the `n` largest integers in list, in ascending order.
+
+    You can assume that `n` will be less than the length of the list.
+
+    For example::
+
+        >>> largest_n_items([2, 6006, 700, 42, 6, 59], 3)
+        [59, 700, 6006]
+
+    It should work when `n` is 0::
+
+        >>> largest_n_items([3, 4, 5], 0)
+        []
+
+    If there are duplicates in the list, they should be counted
+    separately::
+
+        >>> largest_n_items([3, 3, 3, 2, 1], 2)
+        [3, 3]
+    """
+
+    # assign items run through the built in sorted function 
+    # to the sorted_list identifier
+    sorted_list = sorted(items)
+
+    if n == 0:
+        return []
+    else:
+        return sorted_list[-n:]
+
+
+#####################################################################
+# END OF ASSESSMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
